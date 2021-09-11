96ff762f7f92f46f401826d18729ca0ad6aab9a5
toth-b <tothbalazs.contact@gmail.com>
2021-09-11 16:43:12 +0200

    Add icons to Cart and Login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Container, Nav, Navbar, Row} from 'react-bootstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 function Header() {
     return (
@@ -7,11 +8,11 @@ function Header() {
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
                     <Navbar.Brand href="/">eCommerce Site</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/cart">Cart</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/cart"><FontAwesomeIcon icon="shopping-cart"/> Shopping Cart</Nav.Link>
+                            <Nav.Link href="/login"><FontAwesomeIcon icon="user"/> Login</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
