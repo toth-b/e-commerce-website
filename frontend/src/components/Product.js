a1d649cb5583165f4e7d9258fca950864df3ad4f
toth-b <tothbalazs.contact@gmail.com>
2021-09-11 17:44:54 +0200

    Create Product component and fill with data to render on the homepage

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+
+function Product({product}) {
+    return (
+        <Card className="my-3 p-3 rounded">
+            {/* Product Image */}
+            <a href={`/product/${product.id}`}>
+                <Card.Img src={product.image}/>
+            </a>
+
+            <Card.Body>
+                {/* Product Title */}
+                <a href={`/product/${product.id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                {/* Product rating and reviews */}
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {product.rating} from {product.numReviews} reviews
+                    </div>
+                </Card.Text>
+
+                {/* Product  */}
+                <Card.Text as="h3">
+                    {product.price}$
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Product
