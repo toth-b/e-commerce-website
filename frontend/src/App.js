f8449fd62aced9e81c6a2eab6459bc0b1aa25e8a
toth-b <tothbalazs.contact@gmail.com>
2021-09-11 16:42:52 +0200

    Create a dynamic library to store the icon svg components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Container} from 'react-bootstrap'
+// Library Core:
+import {library} from '@fortawesome/fontawesome-svg-core'
+// Brand Icons:
+import {fab} from '@fortawesome/free-brands-svg-icons'
+// FontAwesome icons used in the project:
+import {faShoppingCart, faUser} from '@fortawesome/free-solid-svg-icons'
+// Collect the icons into the library:
+library.add(fab, faShoppingCart, faUser)
 
 function App() {
     return (
