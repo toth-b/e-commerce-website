7775826e43274bd34513686e5c463c8d691e9b77
toth-b <tothbalazs.contact@gmail.com>
2021-09-11 16:22:42 +0200

    Add Container component for main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from 'react-bootstrap'
 
 function App() {
-  return (
-    <div>
-      My eCommerce Site
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome</h1>
+                </Container>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
-export default App;
+export default App
