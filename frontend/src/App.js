6452bdcaf7319522b90af73f41d225e640940cf0
toth-b <tothbalazs.contact@gmail.com>
2021-09-11 17:43:12 +0200

    Add HomePage to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
 import {Container} from 'react-bootstrap'
 // Library Core:
 import {library} from '@fortawesome/fontawesome-svg-core'
@@ -16,7 +17,7 @@ function App() {
             <Header/>
             <main className="py-3">
                 <Container>
-                    <h1>Welcome</h1>
+                    <HomePage/>
                 </Container>
             </main>
             <Footer/>
